d77acc7ec14a10ece702b724174ae32e1acc81b2
Clément Désiles <1536672+jokesterfr@users.noreply.github.com>
2023-06-30 17:12:20 +0200

    Add init scripts support

--- tools/sql-restore-and-run-nginx.sh
+++ tools/sql-restore-and-run-nginx.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -euo pipefail
 
-echo "Applying PS_DOMAIN ($PS_DOMAIN) to the dump"
+echo "* Applying PS_DOMAIN ($PS_DOMAIN) to the dump"
 sed -i s/replace-me.com/$PS_DOMAIN/g /dump.sql
 
 # Configure the DBO parameters
@@ -13,15 +13,14 @@ sed -i \
     -e "s/password' => 'prestashop'/password' => '$MYSQL_PASSWORD'/" \
   $PS_FOLDER/app/config/parameters.php
 
-
 # Restoring MySQL dump
 mysql -u ${MYSQL_USER} --host=${MYSQL_HOST} --password=${MYSQL_PASSWORD} ${MYSQL_DATABASE} < /dump.sql
-echo "MySQL dump restored!"
+echo "* MySQL dump restored!"
 
 # Restoring extra MySQL dump if any
 if [ -n "$MYSQL_EXTRA_DUMP" ]; then
+  echo "* Restoring MySQL EXTRA dump(s)..."
   mysql -u ${MYSQL_USER} --host=${MYSQL_HOST} --password=${MYSQL_PASSWORD} ${MYSQL_DATABASE} < $MYSQL_EXTRA_DUMP
-  echo "MySQL EXTRA dump restored!"
 fi
 
 # Debug mode if enabled
@@ -29,9 +28,19 @@ if [ "$DEBUG_MODE" == "true" ] || [ "$DEBUG_MODE" == "1" ]; then
   sed -ie "s/define('_PS_MODE_DEV_', false);/define('_PS_MODE_DEV_',\ true);/g" $PS_FOLDER/config/defines.inc.php
 fi;
 
-echo "Starting php-fpm..."
+# Init scripts
+if [ -d /tmp/init-scripts/ ]; then
+  echo "* Running init script(s)..."
+  for i in `ls /tmp/init-scripts/`;do
+    /tmp/init-scripts/$i
+  done
+else
+  echo "* No init script found, let's continue..."
+fi
+
+echo "* Starting php-fpm..."
 # su www-data -s /usr/local/sbin/php-fpm -c '-D'
 php-fpm -D --allow-to-run-as-root
 
-echo "Starting nginx..."
+echo "* Starting nginx..."
 nginx -g "daemon off;"
