ea8932db0b90ab28a7aca8c0c2bb577e10186061
Vincent Le Quec <vincent.le-quec@prestashop.com>
2023-11-27 13:32:13 +0100

    feat(build): Some improvements

--- build.sh
+++ build.sh
@@ -9,7 +9,6 @@ declare PHP_VERSION;     # -- PHP version, defaults to recommended version for P
 declare OS_FLAVOUR;      # -- either "alpine" (default) or "debian"
 declare SERVER_FLAVOUR;  # -- not implemented, either "nginx" (default) or "apache"
 declare PLATFORM;        # -- a comma separated list of target platforms (defaults to "linux/amd64")
-declare TAG;             # -- overrides automatically generated tag for the docker image
 declare TARGET_IMAGE;    # -- docker image name, defaults to "prestashop/prestashop-flashlight"
 declare PUSH;            # -- set it to "true" if you want to push the resulting image
 
@@ -18,6 +17,7 @@ declare PUSH;            # -- set it to "true" if you want to push the resulting
 DEFAULT_OS="alpine";
 DEFAULT_SERVER="nginx";
 DEFAULT_DOCKER_IMAGE=prestashop/prestashop-flashlight
+DEFAULT_PLATFORM=linux/amd64
 GIT_SHA=$(git rev-parse HEAD)
 
 error() {
@@ -50,21 +50,6 @@ get_php_flavour() {
    jq -r '."'"${PHP_VERSION}"'".'"${OS_FLAVOUR}" <php-flavours.json;
 }
 
-get_tag() {
-  local TAG=${1:-};
-  local PS_VERSION=${2:-};
-  local PHP_VERSION=${3:-};
-  if [ "$PS_VERSION" == "latest" ] && [ "$PHP_VERSION" == "latest" ]; then
-    echo "latest";
-  elif [ -z "$PS_VERSION" ] && [ -z "$PHP_VERSION" ]; then
-    echo "latest";
-  elif [ -n "$TAG" ]; then
-    echo "$TAG";
-  else
-    echo "auto"
-  fi
-}
-
 get_ps_version() {
   local PS_VERSION=${1:-};
   if [ -z "$PS_VERSION" ] || [ "$PS_VERSION" == "latest" ] ; then
@@ -79,24 +64,31 @@ get_php_version() {
   local PS_VERSION=${2:-};
   if [ -z "$PHP_VERSION" ] || [ "$PHP_VERSION" == "latest" ] ; then
     get_recommended_php_version "$PS_VERSION"
-  else 
+  else
     echo "$PHP_VERSION";
   fi
 }
 
+#
+# if the build is for the latest image of the default OS with the recommended PHP version, these tags will be like:
+# * latest
+# * 8.1.1
+# * 8.1.1-8.2
+# * 8.1.1-8.2-alpine
+#
 get_target_images() {
-  local TAG=${1:-};
-  local PHP_FLAVOUR=${2:-};
-  local PS_VERSION=${3:-};
-  local PHP_VERSION=${4:-};
-  local OS_FLAVOUR=${5:-};
-  local LATEST=${6:-};
+  local PHP_FLAVOUR=${1:-};
+  local PS_VERSION=${2:-};
+  local PHP_VERSION=${3:-};
+  local OS_FLAVOUR=${4:-};
   declare RES;
-  [ "$LATEST" = "true" ] && [ "$OS_FLAVOUR" = "alpine" ] && RES="-t ${DEFAULT_DOCKER_IMAGE}:latest";
+  if [ "$PS_VERSION" = "$(get_latest_prestashop_version)" ] && [ "$OS_FLAVOUR" = "$DEFAULT_OS" ]; then
+    RES="-t ${DEFAULT_DOCKER_IMAGE}:latest";
+  fi
   if [ "$OS_FLAVOUR" = "$DEFAULT_OS" ]; then
     RES="${RES} -t ${DEFAULT_DOCKER_IMAGE}:${PS_VERSION}-${PHP_VERSION}";
     if [ "$PHP_VERSION" = "$(get_recommended_php_version "$PS_VERSION")" ]; then
-      RES="${RES} -t ${DEFAULT_DOCKER_IMAGE}:${PS_VERSION}"; 
+      RES="${RES} -t ${DEFAULT_DOCKER_IMAGE}:${PS_VERSION}";
     fi
   fi
   RES="${RES} -t ${DEFAULT_DOCKER_IMAGE}:${PS_VERSION}-${PHP_FLAVOUR}";
@@ -106,15 +98,11 @@ get_target_images() {
 
 # Applying configuration
 # ----------------------
-TAG=$(get_tag "$TAG" "$PS_VERSION" "$PHP_VERSION");
 PS_VERSION=$(get_ps_version "$PS_VERSION");
 PHP_VERSION=$(get_php_version "$PHP_VERSION" "$PS_VERSION");
 if [ -z "$PHP_VERSION" ]; then
   error "Could not find a recommended PHP version for PS_VERSION: $PS_VERSION" 2
 fi
-declare LATEST;
-[ "$TAG" = "latest" ] && LATEST="true";
-[ "$TAG" = "auto" ] && LATEST="true" && TAG="$PS_VERSION-$PHP_VERSION";
 OS_FLAVOUR=${OS_FLAVOUR:-$DEFAULT_OS};
 SERVER_FLAVOUR=${SERVER_FLAVOUR:-$DEFAULT_SERVER};
 PHP_FLAVOUR=$(get_php_flavour "$OS_FLAVOUR" "$SERVER_FLAVOUR" "$PHP_VERSION");
@@ -122,7 +110,7 @@ if [ "$PHP_FLAVOUR" == "null" ]; then
   error "Could not find a PHP flavour for $OS_FLAVOUR + $SERVER_FLAVOUR + $PHP_VERSION" 2;
 fi
 if [ -z "${TARGET_IMAGE:+x}" ]; then
-  read -ra TARGET_IMAGES <<<"$(get_target_images "$TAG" "$PHP_FLAVOUR" "$PS_VERSION" "$PHP_VERSION" "$OS_FLAVOUR" "$LATEST")"
+  read -ra TARGET_IMAGES <<<"$(get_target_images "$PHP_FLAVOUR" "$PS_VERSION" "$PHP_VERSION" "$OS_FLAVOUR")"
 else
   read -ra TARGET_IMAGES <<<"-t $TARGET_IMAGE"
 fi
@@ -133,8 +121,9 @@ CACHE_IMAGE=${TARGET_IMAGES[1]}
 docker pull "$CACHE_IMAGE" 2> /dev/null || true
 docker buildx build \
   --file "./docker/${OS_FLAVOUR}.Dockerfile" \
-  --platform "${PLATFORM:-linux/amd64}" \
-  --cache-from "$CACHE_IMAGE" \
+  --platform "${PLATFORM:-$DEFAULT_PLATFORM}" \
+  --cache-from type=registry,ref="$CACHE_IMAGE" \
+  --cache-to type=inline \
   --build-arg PHP_FLAVOUR="$PHP_FLAVOUR" \
   --build-arg PS_VERSION="$PS_VERSION" \
   --build-arg PHP_VERSION="$PHP_VERSION" \
