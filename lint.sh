16b96542b3018bc947a96428f3cf00edf0004ae9
Benoit Houdayer <benoit.houdayer@prestashop.com>
2024-01-17 16:23:33 +0100

    fix lint & lint script

--- lint.sh
+++ lint.sh
@@ -3,7 +3,7 @@ set -e
 cd "$(dirname "$0")"
 
 # Lint bash scripts
-find . -type f -name '*.sh' -print0 | xargs -0 shellcheck -x -s bash;
+find . -type f -name '*.sh' -print0 | xargs -0 shellcheck -x;
 
 # Lint docker files
 find . -type f -name '*.Dockerfile' -print0 | xargs -0 hadolint;
