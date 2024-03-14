bb2bee5cd731a38130ac27ab68bbef5d7e1f3103
Clément Désiles <1536672+jokesterfr@users.noreply.github.com>
2024-03-14 07:32:31 +0100

    Important rework of the code structure to lower the images footprint

---
+++ assets/alpine-base-install.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+set -eu
+
+# Install base tools, PHP requirements and dev-tools
+apk --no-cache add -U \
+  bash less vim geoip git tzdata zip curl jq \
+  nginx nginx-mod-http-headers-more nginx-mod-http-geoip \
+  nginx-mod-stream nginx-mod-stream-geoip ca-certificates \
+  gnu-libiconv php-common mariadb-client sudo libjpeg libxml2 \
+  build-base linux-headers freetype-dev zlib-dev libjpeg-turbo-dev \
+  libpng-dev oniguruma-dev libzip-dev icu-dev libmcrypt-dev libxml2-dev
+
+# Configure php-fpm and nginx
+/tmp/php-configuration.sh
+rm -rf /var/log/php* /etc/php*/php-fpm.conf /etc/php*/php-fpm.d
+mkdir -p /var/log/php /var/run/php /var/run/nginx
+chown nginx:nginx /var/run/nginx
+chown www-data:www-data /var/log/php /var/run/php
+
+# Install composer
+curl -s https://getcomposer.org/installer | php
+mv composer.phar /usr/bin/composer
+
+# Compute the short version (8.1.27 becomes 8.1)
+PHP_SHORT_VERSION=$(echo "$PHP_VERSION" | cut -d '.' -f1-2)
+
+# Install phpunit
+PHPUNIT_VERSION=$(jq -r '."'"${PHP_SHORT_VERSION}"'".phpunit' < /tmp/php-flavours.json)
+wget -q -O /usr/bin/phpunit "https://phar.phpunit.de/phpunit-${PHPUNIT_VERSION}.phar"
+chmod +x /usr/bin/phpunit
+
+# Install phpstan
+PHPSTAN_VERSION=$(jq -r '."'"${PHP_SHORT_VERSION}"'".phpstan' < /tmp/php-flavours.json)
+wget -q -O /usr/bin/phpstan "https://github.com/phpstan/phpstan/raw/${PHPSTAN_VERSION}/phpstan.phar"
+chmod a+x /usr/bin/phpstan
+
+# Install php-cs-fixer
+PHP_CS_FIXER=$(jq -r '."'"${PHP_SHORT_VERSION}"'".php_cs_fixer' < /tmp/php-flavours.json)
+wget -q -O /usr/bin/php-cs-fixer "https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/releases/download/${PHP_CS_FIXER}/php-cs-fixer.phar"
+chmod a+x /usr/bin/php-cs-fixer
+
+# Install xdebug
+PHP_XDEBUG=$(jq -r '."'"${PHP_SHORT_VERSION}"'".xdebug' < /tmp/php-flavours.json)
+pecl install "xdebug-$PHP_XDEBUG"
+docker-php-ext-enable xdebug
+
+# Install Node.js (shipping yarn and npm) and pnpm
+if [ "0.0.0" != "$NODE_VERSION" ]; then
+  apk --no-cache add -U python3 nodejs npm yarn
+  npm install -g pnpm@latest
+fi
+
+# Cleanup dev packages, keep libraries
+apk --no-cache del -U build-base gcc g++ libgcc nginx-vim mariadb xz-dev musl-dev linux-headers freetype-dev zlib-dev libjpeg-turbo-dev libpng-dev oniguruma-dev libzip-dev icu-dev libmcrypt-dev libxml2-dev
+apk --no-cache add -U mariadb-client freetype zlib libjpeg-turbo libpng oniguruma libzip icu libmcrypt libxml2
+rm -rf /var/cache/apk/*
---
+++ assets/debian-base-install.sh
@@ -0,0 +1,87 @@
+#!/bin/bash
+set -eu
+
+# Disable man pages and documentation
+tee -a /etc/dpkg/dpkg.cfg.d/01_nodoc << END
+path-exclude=/usr/share/locale/*
+path-exclude=/usr/share/man/*
+path-exclude=/usr/share/doc/*
+path-include=/usr/share/doc/*/copyright
+END
+rm -rf /usr/share/doc \
+  /usr/share/man \
+  /usr/share/locale
+
+# Update certificates and install base deps
+export DEBIAN_FRONTEND=noninteractive
+apt-key adv --fetch-keys 'https://packages.sury.org/php/apt.gpg' > /dev/null 2>&1
+apt-get update
+apt-get install --no-install-recommends -qqy ca-certificates
+apt-get install --no-install-recommends -o Dpkg::Options::="--force-confold" -qqy bash less vim git sudo mariadb-client \
+  tzdata zip unzip curl wget make jq netcat-traditional build-essential \
+  lsb-release libgnutls30 gnupg libiconv-hook1 libonig-dev nginx libnginx-mod-http-headers-more-filter libnginx-mod-http-geoip \
+  libnginx-mod-http-geoip libnginx-mod-stream;
+
+# shellcheck disable=SC1091
+. /etc/os-release
+echo "deb [trusted=yes] https://packages.sury.org/php/ $VERSION_CODENAME main" > /etc/apt/sources.list.d/php.list
+rm /etc/apt/preferences.d/no-debian-php
+apt-get update
+apt-get install --no-install-recommends -qqy \
+  php-gd \
+  libfreetype-dev \
+  libghc-zlib-dev \
+  libjpeg-dev \
+  libpng-dev \
+  libzip-dev \
+  libicu-dev \
+  libmcrypt-dev \
+  libxml2-dev \
+
+# Configure php-fpm and nginx
+/tmp/php-configuration.sh
+rm -rf /var/log/php* /etc/php*/php-fpm.conf /etc/php*/php-fpm.d
+mkdir -p /var/log/php /var/run/php /var/run/nginx
+adduser --group nginx
+adduser --system nginx
+chown nginx:nginx /var/run/nginx
+chown www-data:www-data /var/log/php /var/run/php
+
+# Install composer
+curl -s https://getcomposer.org/installer | php \
+  && mv composer.phar /usr/bin/composer
+
+# Compute the short version (8.1.27 becomes 8.1)
+PHP_SHORT_VERSION=$(echo "$PHP_VERSION" | cut -d '.' -f1-2)
+
+# Install phpunit
+PHPUNIT_VERSION=$(jq -r '."'"${PHP_SHORT_VERSION}"'".phpunit' < /tmp/php-flavours.json)
+wget -q -O /usr/bin/phpunit "https://phar.phpunit.de/phpunit-${PHPUNIT_VERSION}.phar"
+chmod +x /usr/bin/phpunit
+
+# Install phpstan
+PHPSTAN_VERSION=$(jq -r '."'"${PHP_SHORT_VERSION}"'".phpstan' < /tmp/php-flavours.json)
+wget -q -O /usr/bin/phpstan "https://github.com/phpstan/phpstan/raw/${PHPSTAN_VERSION}/phpstan.phar"
+chmod a+x /usr/bin/phpstan
+
+# Install php-cs-fixer
+PHP_CS_FIXER=$(jq -r '."'"${PHP_SHORT_VERSION}"'".php_cs_fixer' < /tmp/php-flavours.json)
+wget -q -O /usr/bin/php-cs-fixer "https://github.com/PHP-CS-Fixer/PHP-CS-Fixer/releases/download/${PHP_CS_FIXER}/php-cs-fixer.phar"
+chmod a+x /usr/bin/php-cs-fixer
+
+# Install xdebug
+PHP_XDEBUG=$(jq -r '."'"${PHP_SHORT_VERSION}"'".xdebug' < /tmp/php-flavours.json)
+pecl install "xdebug-${PHP_XDEBUG}"
+docker-php-ext-enable xdebug
+
+# Install Node.js (shipping yarn and npm) and pnpm
+if [ "0.0.0" != "$NODE_VERSION" ]; then
+  apt-get install --no-install-recommends -qqy nodejs python3 npm
+  npm install -g yarn@latest pnpm@latest --force
+fi
+
+# Cleanup dev packages, keep libraries
+apt-get clean
+apt-get purge -qqy build-essential libfreetype-dev \
+  libghc-zlib-dev libjpeg-dev libpng-dev libzip-dev libicu-dev libmcrypt-dev libxml2-dev
+rm -rf /var/lib/apt/lists/*
