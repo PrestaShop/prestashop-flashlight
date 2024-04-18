213ab476da8dc3befe3da0dddf4478a7cfd9bbb0
Clément Désiles <1536672+jokesterfr@users.noreply.github.com>
2024-04-18 17:48:47 +0200

    feat(build): add github-cli (#95)

--- assets/debian-base-install.sh
+++ assets/debian-base-install.sh
@@ -85,6 +85,9 @@ if [ "0.0.0" != "$NODE_VERSION" ]; then
   npm install -g yarn@latest pnpm@latest --force
 fi
 
+# Install github-cli
+apt-get install --no-install-recommends -qqy gh || curl -sS https://webi.sh/gh | sh
+
 # Cleanup dev packages, keep libraries
 apt-get clean
 apt-get purge -qqy build-essential gcc g++ ghc "$LIB_FREETYPE_DEV" linux-libc-dev libncurses-dev \
