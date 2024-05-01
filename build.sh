fff17fe2a9f67596076e6b44066a74560ba3e792
Alexandre Lematre <alexandre.lematre@prestashop.com>
2024-05-01 11:17:02 +0200

    Code review fixes

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ declare PLATFORM;          # -- alias for $TARGET_PLATFORM
 declare TARGET_IMAGE;      # -- docker image name, defaults to "prestashop/prestashop-flashlight"
 declare PUSH;              # -- set it to "true" if you want to push the resulting image
 declare ZIP_SOURCE;        # -- the zip to unpack in flashlight
-declare INSTALL_MODULES;   # -- install modules during zip installation
+declare PRE_INSTALLED_MODULES;   # -- install modules during zip installation
 declare DRY_RUN;           # -- if used, won't really build the image. Useful to check tags compliance
 declare CUSTOM_LABELS;     # -- only when PRIVATE : list of key=value pairs separated by a comma, for overriding official flashlight labels
 
@@ -192,14 +192,15 @@ if [ -n "${DRY_RUN}" ]; then
   }
 fi
 
-labelString=
+COMPUTED_LABELS=()
 for key in "${!IMAGE_LABELS[@]}"
 do
-  labelString=$labelString' --label '$key'="'${IMAGE_LABELS[$key]}'"'
+  COMPUTED_LABELS+=("--label")
+  COMPUTED_LABELS+=("$key=\"${IMAGE_LABELS[$key]}\"")
 done
 
 docker pull "$CACHE_IMAGE" 2> /dev/null || true
-eval docker buildx build \
+docker buildx build \
   --progress=plain \
   --file "./docker/${OS_FLAVOUR}.Dockerfile" \
   --platform "$TARGET_PLATFORM" \
@@ -211,8 +212,8 @@ eval docker buildx build \
   --build-arg GIT_SHA="$GIT_SHA" \
   --build-arg NODE_VERSION="$NODE_VERSION" \
   --build-arg ZIP_SOURCE="$ZIP_SOURCE" \
-  --build-arg INSTALL_MODULES="$INSTALL_MODULES" \
-  "$labelString" \
-  "${TARGET_IMAGES[*]}" \
+  --build-arg PRE_INSTALLED_MODULES="$PRE_INSTALLED_MODULES" \
+  "${COMPUTED_LABELS[@]}" \
+  "${TARGET_IMAGES[@]}" \
   "$([ "${PUSH}" == "true" ] && echo "--push" || echo "--load")" \
   .
