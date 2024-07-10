3ada26fc170e510bb0d9a550885a88cfb2613dea
Clément Désiles <1536672+jokesterfr@users.noreply.github.com>
2024-07-10 16:15:23 +0200

    fix up linting

--- release.sh
+++ release.sh
@@ -2,34 +2,34 @@
 set -eu
 EXCLUDED_TAGS='\/1.5|\/1.6.0|alpha|beta|rc|RC|\^'
 PRESTASHOP_TAGS=$(git ls-remote --tags git@github.com:PrestaShop/PrestaShop.git | cut -f2 | grep -Ev $EXCLUDED_TAGS | cut -d '/' -f3 | sort -r -V)
-PRESTASHOP_MAJOR_TAGS=$(
-  MAJOR_TAGS=""
-  for VERSION in $PRESTASHOP_TAGS; do
-    CRITERIA=$(echo "$VERSION" | cut -d. -f1)
-    # shellcheck disable=SC3010
-    if [[ "$CRITERIA" == 1* ]]; then
-      CRITERIA=$(echo "$VERSION" | cut -d. -f1-2)
-    fi
-    if ! echo "$MAJOR_TAGS" | grep -q "^$CRITERIA"; then
-      MAJOR_TAGS="$MAJOR_TAGS\n$VERSION";
-    fi
-  done
-  echo "$MAJOR_TAGS"
-)
-PRESTASHOP_MINOR_TAGS=$(
-  MINOR_TAGS=""
-  for VERSION in $PRESTASHOP_TAGS; do
-    CRITERIA=$(echo "$VERSION" | cut -d. -f1-2)
-    # shellcheck disable=SC3010
-    if [[ "$CRITERIA" == 1* ]]; then
-      CRITERIA=$(echo "$VERSION" | cut -d. -f1-3)
-    fi
-    if ! echo "$MINOR_TAGS" | grep -q "^$CRITERIA"; then
-      MINOR_TAGS="$MINOR_TAGS\n$VERSION";
-    fi
-  done
-  echo "$MINOR_TAGS"
-)
+# PRESTASHOP_MAJOR_TAGS=$(
+#   MAJOR_TAGS=""
+#   for VERSION in $PRESTASHOP_TAGS; do
+#     CRITERIA=$(echo "$VERSION" | cut -d. -f1)
+#     # shellcheck disable=SC3010
+#     if [[ "$CRITERIA" == 1* ]]; then
+#       CRITERIA=$(echo "$VERSION" | cut -d. -f1-2)
+#     fi
+#     if ! echo "$MAJOR_TAGS" | grep -q "^$CRITERIA"; then
+#       MAJOR_TAGS="$MAJOR_TAGS\n$VERSION";
+#     fi
+#   done
+#   echo "$MAJOR_TAGS"
+# )
+# PRESTASHOP_MINOR_TAGS=$(
+#   MINOR_TAGS=""
+#   for VERSION in $PRESTASHOP_TAGS; do
+#     CRITERIA=$(echo "$VERSION" | cut -d. -f1-2)
+#     # shellcheck disable=SC3010
+#     if [[ "$CRITERIA" == 1* ]]; then
+#       CRITERIA=$(echo "$VERSION" | cut -d. -f1-3)
+#     fi
+#     if ! echo "$MINOR_TAGS" | grep -q "^$CRITERIA"; then
+#       MINOR_TAGS="$MINOR_TAGS\n$VERSION";
+#     fi
+#   done
+#   echo "$MINOR_TAGS"
+# )
 
 get_compatible_php_version() {
   REGEXP_LIST=$(echo "$PRESTASHOP_TAGS" | jq -r 'keys_unsorted | .[]')
@@ -54,20 +54,14 @@ publish() {
 publish --field ps_version=latest
 publish --field ps_version=latest --field os_flavour=debian
 
-# Recommended PHP for some minor tag
-# for PS_VERSION in $PRESTASHOP_MINOR_TAGS; do
-#   publish --field ps_version="$PS_VERSION"
-#   publish --field ps_version="$PS_VERSION" --field os_flavour=debian
-# done
-
-# Recommended PHP for every tag
+# Build & publish every prestashop version with recommended PHP version
 for PS_VERSION in $PRESTASHOP_TAGS; do
   publish --field ps_version="$PS_VERSION"
   publish --field ps_version="$PS_VERSION" --field os_flavour=debian
 done
 
-# Compatible PHP for every minor tag (alpine only)
-# for PS_VERSION in $PRESTASHOP_TAGS; do
+# Build & publish every prestashop minor version with all compatible PHP versions (alpine only)
+# for PS_VERSION in $PRESTASHOP_MINOR_TAGS; do
 #   while IFS= read -r PHP_VERSION; do
 #     publish --field ps_version="$PS_VERSION" --field php_version="$PHP_VERSION"
 #   done <<EOF
