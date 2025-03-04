0ef2dbb151e8202c40163cc6ba474c2e3d8f16df
Alexandre Lematre <alexandre.lematre@prestashop.com>
2025-03-04 09:36:17 +0100

    Fixed release.sh

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 set -eu
 EXCLUDED_TAGS='\/1.5|\/1.6.0|alpha|beta|rc|RC|\^'
 PRESTASHOP_TAGS=$(git ls-remote --tags git@github.com:PrestaShop/PrestaShop.git | cut -f2 | grep -Ev $EXCLUDED_TAGS | cut -d '/' -f3 | sort -r -V)
@@ -18,7 +18,7 @@ PRESTASHOP_TAGS_DEBIAN=$(echo "$PRESTASHOP_TAGS" | grep -Ev '^1.7|1.6')
 #   echo "$MAJOR_TAGS"
 # )
 PRESTASHOP_MINOR_TAGS=$(
-  MINOR_TAGS=""
+  MINOR_TAGS=$()
   for VERSION in $PRESTASHOP_TAGS; do
     CRITERIA=$(echo "$VERSION" | cut -d. -f1-2)
     # shellcheck disable=SC3010
@@ -26,18 +26,18 @@ PRESTASHOP_MINOR_TAGS=$(
       CRITERIA=$(echo "$VERSION" | cut -d. -f1-3)
     fi
     if ! echo "$MINOR_TAGS" | grep -q "^$CRITERIA"; then
-      MINOR_TAGS="$MINOR_TAGS\n$VERSION";
+      MINOR_TAGS+=("$VERSION");
     fi
   done
-  echo "$MINOR_TAGS"
+  echo "${MINOR_TAGS[@]}"
 )
 
 get_compatible_php_version() {
-  REGEXP_LIST=$(echo "$PRESTASHOP_TAGS" | jq -r 'keys_unsorted | .[]')
+  REGEXP_LIST=$(cat prestashop-versions.json | jq -r 'keys_unsorted | .[]')
   while IFS= read -r regExp; do
     # shellcheck disable=SC3010
     if [[ $1 =~ $regExp ]]; then
-      echo "$PRESTASHOP_TAGS" | jq -r '."'"${regExp}"'".php.compatible[]'
+      cat prestashop-versions.json | jq -r '."'"${regExp}"'".php.compatible[]'
       break;
     fi
   done <<EOF
@@ -47,9 +47,9 @@ EOF
 
 publish() {
   echo "Publishing" "$@"
-  gh workflow run docker-publish.yml \
-    --repo prestashop/prestashop-flashlight \
-    --field target_platforms=linux/amd64,linux/arm64 "$@"
+  # gh workflow run docker-publish.yml \
+  #   --repo prestashop/prestashop-flashlight \
+  #   --field target_platforms=linux/amd64,linux/arm64 "$@"
 }
 
 # Latest
